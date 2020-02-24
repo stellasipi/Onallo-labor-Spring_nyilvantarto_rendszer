c1c8399eac29d330f80c4168c74f4a8113acb6d5
Stella Tóth-Baranyi <stellasipi@gmail.com>
2020-02-24 22:43:49 +0100

    Spring init

---
+++ src/main/java/hu/bme/vik/tbs/onlab/CsotthonApp/CsotthonAppApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.vik.tbs.onlab.CsotthonApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsotthonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsotthonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bme/vik/tbs/onlab/CsotthonApp/CsotthonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.vik.tbs.onlab.CsotthonApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsotthonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
