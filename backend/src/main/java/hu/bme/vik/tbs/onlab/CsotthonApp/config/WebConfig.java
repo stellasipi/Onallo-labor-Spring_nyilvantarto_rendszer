930effe9cb855da63042a5582e617469fd2c5131
Stella Tóth-Baranyi <stellasipi@gmail.com>
2020-03-22 17:15:36 +0100

    Fix timestamp displaying

--- backend/src/main/java/hu/bme/vik/tbs/onlab/CsotthonApp/config/WebConfig.java
+++ backend/src/main/java/hu/bme/vik/tbs/onlab/CsotthonApp/config/WebConfig.java
@@ -2,7 +2,6 @@ package hu.bme.vik.tbs.onlab.CsotthonApp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,4 +11,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
 }
--- backend/src/main/java/hu/bme/vik/tbs/onlab/CsotthonApp/model/Log.java
+++ backend/src/main/java/hu/bme/vik/tbs/onlab/CsotthonApp/model/Log.java
@@ -1,11 +1,8 @@
 package hu.bme.vik.tbs.onlab.CsotthonApp.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonView;
 import hu.bme.vik.tbs.onlab.CsotthonApp.util.LogType;
 import lombok.*;
-import org.springframework.web.servlet.View;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
