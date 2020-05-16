8089b83f1d59b8acdafbf7bc2adf690365c1f254
Stella Tóth-Baranyi <stellasipi@gmail.com>
2020-05-16 08:45:45 +0200

    Fix month in time

--- src/main/java/hu/bme/vik/tbs/onlab/CsotthonApp/util/Time.java
+++ src/main/java/hu/bme/vik/tbs/onlab/CsotthonApp/util/Time.java
@@ -1,29 +1,13 @@
 package hu.bme.vik.tbs.onlab.CsotthonApp.util;
 
 import java.sql.Timestamp;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Time {
     public static Timestamp getNowInUTC(){
-        return new Timestamp(ZonedDateTime.of(
-                Calendar.getInstance().get(Calendar.YEAR),
-                Calendar.getInstance().get(Calendar.MONTH),
-                Calendar.getInstance().get(Calendar.DAY_OF_MONTH),
-                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
-                Calendar.getInstance().get(Calendar.MINUTE),
-                0, 0,
-                ZoneId.of("UTC")).toInstant().toEpochMilli());
+        return  new Timestamp(System.currentTimeMillis());
     }
     public static Timestamp getSpecificTimeInUTC(Integer year,Integer month, Integer day, Integer hour, Integer minute){
-        return new Timestamp(ZonedDateTime.of(
-                year,
-                month,
-                day,
-                hour,
-                minute,
-                0, 0,
-                ZoneId.of("UTC")).toInstant().toEpochMilli());
+        return new Timestamp(new GregorianCalendar(year, month, day,hour,minute,00).getTimeInMillis());
     }
 }
